db51ed22db1b8a9b8e61a22f466d6222495d8136
Jhonny Quispe Navarro <jhonny.quispejl@gmail.com>
2020-11-18 17:56:22 -0500

    Carousel with infinite-react-carousel

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+
+// components
+import Slider from './components/Slider'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const images = [
+		{
+			id: '1',
+			title: 'Awesome forest',
+			image:
+				'https://cdn.pixabay.com/photo/2020/11/09/15/12/trail-5726987_960_720.jpg',
+		},
+		{
+			id: '2',
+			title: 'A litle bird.',
+			image:
+				'https://cdn.pixabay.com/photo/2011/09/27/18/52/bird-9950_960_720.jpg',
+		},
+		{
+			id: '3',
+			title: 'The best friend.',
+			image:
+				'https://cdn.pixabay.com/photo/2017/12/27/14/02/friends-3042751_960_720.jpg',
+		},
+		{
+			id: '4',
+			title: 'Beautiful desert.',
+			image:
+				'https://cdn.pixabay.com/photo/2016/11/21/17/44/arches-national-park-1846759_960_720.jpg',
+		},
+		{
+			id: '5',
+			title: 'Harley Davidson motorcycle.',
+			image:
+				'https://cdn.pixabay.com/photo/2018/10/26/22/55/harley-davidson-3775527_960_720.jpg',
+		},
+	]
+
+	return (
+		<div className='App'>
+			<Slider images={images} />
+		</div>
+	)
 }
 
-export default App;
+export default App
